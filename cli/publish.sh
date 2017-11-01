19694ffd27ee073b3afe7007a06b5f99e2594088
lianruhe <lianruhe2013@gmail.com>
2017-11-01 18:16:12 +0800

    :red_car: initial

---
+++ cli/publish.sh
@@ -0,0 +1,23 @@
+set -e
+echo "Enter message: "
+read MESSAGE
+
+echo "Deploying $MESSAGE ..."
+
+clean
+hexo clean
+
+# compiler
+hexo g
+
+cp README.md ./dist/README.md
+
+# commit
+cd dist
+git init
+git add -A
+git commit -m "$MESSAGE"
+git push -f git@github.com:lianruhe/lianruhe.github.io.git master:master
+
+# back to root
+cd ..
